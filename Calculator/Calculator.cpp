c8d3f996988dcb8fbee90686ca97028a1f881db3
mimif10 <94880765+mimif10@users.noreply.github.com>
2022-06-22 16:25:57 -0400

    Merged ButtonFactory branch into master Branch

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,5 +1,40 @@
 #include "Calculator.h"
 
+// implement Event Table (begin and end)
+wxBEGIN_EVENT_TABLE(Calculator, wxFrame) // takes name of the class for the event and the base class)
+// Declare the event button  macroe event button - takes in the first button's windows ID, 
+EVT_BUTTON(200, Calculator::OnButtonClicked) // and link each button eith the ID being clicked to the function we want to call 
+
+// 1st row
+EVT_BUTTON(0, Calculator::OnButtonClicked)
+EVT_BUTTON(1, Calculator::OnButtonClicked)
+EVT_BUTTON(2, Calculator::OnButtonClicked)
+EVT_BUTTON(3, Calculator::OnButtonClicked)
+EVT_BUTTON(1200, Calculator::OnButtonClicked) // Add button
+
+// 2nd row
+EVT_BUTTON(300, Calculator::OnButtonClicked) // dec button
+EVT_BUTTON(4, Calculator::OnButtonClicked)
+EVT_BUTTON(5, Calculator::OnButtonClicked)
+EVT_BUTTON(6, Calculator::OnButtonClicked)
+EVT_BUTTON(1100, Calculator::OnButtonClicked) // substract button
+
+//3rd row
+EVT_BUTTON(400, Calculator::OnButtonClicked) // hex button
+EVT_BUTTON(7, Calculator::OnButtonClicked)
+EVT_BUTTON(8, Calculator::OnButtonClicked)
+EVT_BUTTON(9, Calculator::OnButtonClicked)
+EVT_BUTTON(1000, Calculator::OnButtonClicked) // multiply
+
+// 4th row
+EVT_BUTTON(500, Calculator::OnButtonClicked) // bin
+EVT_BUTTON(600, Calculator::OnButtonClicked) // (-)
+EVT_BUTTON(700, Calculator::OnButtonClicked) // mod
+EVT_BUTTON(800, Calculator::OnButtonClicked) // equal
+EVT_BUTTON(900, Calculator::OnButtonClicked) // divide
+
+wxEND_EVENT_TABLE()
+
 // Window Frame requires some information in order to initialize it (parameters: parent, ID, name, location point, size)
 Calculator::Calculator() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(720, 250), wxSize(320, 340))
 {
@@ -44,4 +79,135 @@ Calculator::~Calculator()
 {
 	
 	
+}
+
+// button Click event - function that handles the button 
+void Calculator::OnButtonClicked(wxCommandEvent& event)
+{
+	int id = event.GetId();
+
+	// Use a Switch Statement for each button
+	switch (id)
+	{
+		case 200: // clear button
+		{
+			textBox->SetValue("0");
+			break;
+		}
+		case 0:
+		{
+			/*if (textBox->GetValue() == "0")
+			{
+			//textBox->SetValue(button0->GetLabel());
+			else
+			{*/
+				textBox->AppendText("0");
+			//}
+			break;
+		}
+		case 1:
+		{
+				textBox->AppendText("1");
+			break;
+		}
+		case 2:
+		{
+				textBox->AppendText("2");
+			break;
+		}
+		case 3:
+		{
+				textBox->AppendText("3");
+			
+			break;
+		}
+		case 1200: // Add button
+		{
+			textBox->AppendText(" + ");
+			break;
+		}
+		case 300: // dec button
+		{
+				textBox->AppendText("dec ");
+			
+			break;
+		}
+		case 4:
+		{
+				textBox->AppendText("4");
+			
+			break;
+		}
+		case 5:
+		{
+				textBox->AppendText("5");
+			
+			break;
+		}
+		case 6:
+		{
+				textBox->AppendText("6");
+			
+			break;
+		}
+		case 1100: // substract button
+		{
+
+			textBox->AppendText(" - ");
+			break;
+		}
+		case 400: // hex button
+		{
+			textBox->AppendText("hex ");
+			break;
+		}
+		case 7:
+		{
+			textBox->AppendText("7");
+
+			break;
+		}
+		case 8:
+		{
+			textBox->AppendText("8");
+
+			break;
+		}
+		case 9:
+		{
+			textBox->AppendText("9");
+
+			break;
+		}
+		case 1000: // multiply button
+		{
+			textBox->AppendText(" * ");
+			break;
+		}
+		case 500: // bin button
+		{
+			textBox->AppendText("bin ");
+			break;
+		}
+		case 600: // neg button
+		{
+			textBox->AppendText("-");
+			break;
+		}
+		case 700: // mod button
+		{
+			textBox->AppendText(" mod ");
+			break;
+		}
+		case 800: // equal button
+		{
+			textBox->AppendText(" = ");
+			break;
+		}
+		case 900: // divide button
+		{
+			textBox->AppendText(" / ");
+			break;
+		}
+	}
 }
