d3b8caf1f3ee5cc5eee567f8e4b401bd240a82c8
mimif10 <94880765+mimif10@users.noreply.github.com>
2022-06-23 00:13:12 -0400

    Created new branhc

---
+++ Calculator/ButtonFactory.h
@@ -0,0 +1,58 @@
+#pragma once
+#include "Calculator.h"
+
+//class Calculator;
+
+class ButtonFactory : wxFrame
+{
+public:
+
+	//Calculator* calculator;
+
+	//Calculator* GetCalculator();
+
+	//wxTextCtrl* CreatetextBox(Calculator* calc);
+
+	////// 1st row
+	//////Calculator* CreateButton1(Calculator* calc);
+
+	//wxButton* CreateButton0(Calculator* calc);// Will create a button
+
+	//wxButton* CreateButton1(Calculator* calc);
+	//wxButton* CreateButton2(Calculator* calc);
+	//wxButton* CreateButton3(Calculator* calc);
+	//wxButton* CreateAddButton(Calculator* calc);
+
+	//// 2nd row
+	//wxButton* CreateDecButton(Calculator* calc);
+	//wxButton* CreateButton4(Calculator* calc);
+	//wxButton* CreateButton5(Calculator* calc);
+	//wxButton* CreateButton6(Calculator* calc);
+	//wxButton* CreateSubstractButton(Calculator* calc);
+
+	////3rd row
+	//wxButton* CreateHexButton(Calculator* calc);
+	//wxButton* CreateButton7(Calculator* calc);
+	//wxButton* CreateButton8(Calculator* calc);
+	//wxButton* CreateButton9(Calculator* calc);
+	//wxButton* CreateMutliplyBtn(Calculator* calc);
+
+	//// 4th row buttons
+	//wxButton* CreateBinButton(Calculator* calc);
+	//wxButton* CreateNegButton(Calculator* calc);
+	//wxButton* CreateModButton(Calculator* calc);
+	//wxButton* CreateEqualButton(Calculator* calc);
+	//wxButton* CreateDivideBtn(Calculator* calc);
+
+	//wxTextCtrl* CreateClearBtn(Calculator* calc);
+
+	//	// wxWidgets provides tools to handle events for us: 
+	//// include the macro declare event table below
+	//wxDECLARE_EVENT_TABLE(); // implement at the top of .cpp file 
+	///*const wxEventTable* GetEventTable() const;
+
+	//wxEventHashTable& GetEventHashTable() const;*/
+
+
+};
+
